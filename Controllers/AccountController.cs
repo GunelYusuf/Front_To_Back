c44e07ae7fea408e758bf39310ce6584d773d006
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2022-01-23 18:04:53 +0400

    fixed Register Action

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FronToBack.Models;
+using FrontToBack.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,13 +13,41 @@ namespace FronToBack.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
 
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
         public IActionResult Register()
         {
             return View();
         }
 
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
 
+        public async Task<IActionResult> Register(RegisterVM register)
+        {
+            if (!ModelState.IsValid) return View();
+            AppUser user = new AppUser
+            {
+                FullName = register.FullName,
+                UserName=register.UserName,
+                Email=register.Email
+
+            };
+            IdentityResult identityResult = await _userManager.CreateAsync(user, register.Password);
+            if (!identityResult.Succeeded)
+            {
+                foreach (var error in identityResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View();
+            }
+            return RedirectToAction("Index", "Home");
+        }
 
 
         public IActionResult Index()
